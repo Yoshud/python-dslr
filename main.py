8a19c62dc48c1392ae805dfa2bd54413556d0353
Yoshud <adam0cisak@gmail.com>
2018-12-19 01:33:29 +0100

    Oczyszczenie z komentarzy

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 import cv2
-# import piggyphoto
-# import pygame
 import gphoto2 as gp
-
-
-# def show(file):
-#     picture = pygame.image.load(file)
-#     main_surface.blit(picture, (0, 0))
-#     pygame.display.flip()
-#
-#
-# camera = gp.gp_camera_new(camera)
-# C = piggyphoto.camera()
-# C.leave_locked()
-# C.capture_preview('p.jpg')
-# img = cv.imread('p.jpg')
-# img
 
 context = gp.gp_context_new()
 error, file = gp.gp_file_new()
@@ -27,16 +11,12 @@ gp.gp_file_save(file, "photo.jpg")
 print('Summary')
 print('=======')
 print(text.text)
-# error = gp.gp_camera_trigger_capture(camera, context)
-# error, filePath = gp.gp_camera_capture(camera, 1, context)
 print(gp.gp_result_as_string(error))
-# gp.gp_camera_file_get()
 
 print("start")
 try:
     for i in range(300):
         error, file = gp.gp_camera_capture_preview(camera, context)
-        # error, filePath = gp.gp_camera_capture(camera, 0, context)
         print(gp.gp_result_as_string(error))
         gp.gp_file_save(file, "photo.jpg")
         photo = cv2.imread("photo.jpg")
@@ -50,10 +30,3 @@ finally:
     cv2.destroyAllWindows()
     error = gp.gp_camera_exit(camera, context)
 
-# picture = pygame.image.load("p.jpg")
-# pygame.display.set_mode(picture.get_size())
-# main_surface = pygame.display.get_surface()
-#
-# while True:
-#     C.capture_preview('p.jpg')
-#     show("p.jpg")
